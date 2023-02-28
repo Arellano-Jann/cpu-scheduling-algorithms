0aedac6a0dda869ea8d1b673b330034bc695c2e7
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-02-27 23:43:28 -0800

    added necessary files

---
+++ batchSchedulingComparison.py
@@ -0,0 +1,17 @@
+import sys
+
+def shortestRemainingSort():
+    pass
+
+def roundRobinSort():
+    pass
+
+def computeStat():
+    pass
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
